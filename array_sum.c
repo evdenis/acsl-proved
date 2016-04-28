b8fdca436c3cade4211f431cc137e6dfef6c6cb9
Denis Efremov <yefremov.denis@gmail.com>
2016-04-28 13:05:35 +0300

    array sum && sum

---
+++ array_sum.c
@@ -0,0 +1,40 @@
+/*@ axiomatic Sum {
+    logic integer sum{L}(unsigned *a, integer b);
+
+    axiom init{L}:
+       \forall unsigned *a, integer b; b <= 0 ==> sum(a, b) == 0;
+
+    axiom step_dec{L}:
+       \forall unsigned *a, integer b; sum(a, b) == sum(a, b-1) + a[b];
+
+    lemma lower_bound:
+       \forall unsigned *a, integer b; sum(a, b) >= 0;
+
+    lemma sum_increases:
+       \forall unsigned *a, integer i, b; 0 <= i <= b ==> sum(a, i) <= sum(a, b);
+    }
+ */
+
+#define UINT_MAX 32767
+
+/*@ requires \valid(a+(0..n-1));
+    requires sum(a, n) < UINT_MAX;
+    assigns \nothing;
+    ensures \result == sum(a, n-1);
+ */
+unsigned sum(unsigned a[], unsigned n)
+{
+   unsigned i;
+   unsigned sum = 0;
+
+   /*@ loop invariant 0 <= i <= n;
+       loop invariant sum == sum(a, i-1);
+       //loop invariant sum(a, i-1) < UINT_MAX; Не обязательно
+       loop variant n - i;
+    */
+   for(i = 0; i < n; ++i) {
+      sum += a[i];
+   }
+
+   return sum;
+}
---
+++ sum.c
@@ -0,0 +1,41 @@
+/*@ axiomatic Sum {
+    logic integer sum(integer a, integer b);
+
+    axiom init:
+       \forall integer a, b; (a >= 0 && b >= 0) && b <= a ==> sum(a, b) == a;
+
+    axiom step_dec:
+       \forall integer a, b; (a >= 0 && b >= 0) && b > a ==> sum(a, b) == sum(a, b - 1) + b;
+
+    lemma lower_bound:
+       \forall integer a, b; a >= 0 && b >= 0 ==> sum(a, b) >= a;
+
+    lemma sum_increases:
+       \forall integer i, a, b; (a >= 0 && b >= 0) && b > a && a <= i <= b ==> sum(a, i) <= sum(a, b);
+    }
+ */
+
+#define INT_MAX 32767
+
+/*@ requires a < b;
+    requires a >= 0 && b >= 0;
+    requires sum(a, b) < INT_MAX;
+    assigns \nothing;
+    ensures \result == sum(a, b);
+ */
+int sum(int a, int b)
+{
+   int i;
+   int sum = a;
+
+   /*@ loop invariant a < i <= (b+1);
+       loop invariant sum == sum(a, i - 1);
+       //loop invariant sum(a, i - 1) < INT_MAX; Доказывается и без него
+       loop variant b - i;
+    */
+   for(i = a + 1; i <= b; ++i) {
+      sum += i;
+   }
+
+   return sum;
+}
