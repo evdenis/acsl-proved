e97b62bd569ee94bdad1f06aee78546976e49927
Denis Efremov <yefremov.denis@gmail.com>
2016-04-29 20:58:51 +0300

    palindrome

---
+++ palindrome.c
@@ -0,0 +1,33 @@
+/*@ requires \valid(a+(0..size-1));
+    assigns \nothing;
+    ensures \result == 0 || \result == 1;
+    behavior palindrome:
+       assumes \forall integer i; 0 <= i < size ==> a[i] == a[size - i - 1];
+       ensures \result == 1;
+    behavior non_palindrome:
+       assumes \exists integer i; 0 <= i < size && a[i] != a[size - i - 1];
+       ensures \result == 0;
+    complete behaviors;
+    disjoint behaviors;
+ */
+int palindrome(int a[], unsigned size)
+{
+   int res = 1;
+   unsigned i;
+
+   /*@ loop invariant 0 <= i <= size / 2;
+       loop invariant (\forall integer j; 0 <= j < i ==> a[j] == a[size - j - 1]) <==> (res == 1);
+       //loop invariant (a[i] != a[size - i - 1]) <==> (res == 0);
+       loop invariant res == 0 || res == 1;
+       loop assigns res;
+       loop variant size - i;
+    */
+   for(i = 0; i < size / 2; ++i) {
+      if (a[i] != a[size - i - 1]) {
+         res = 0;
+         break;
+      }
+   }
+   return res;
+}
+
