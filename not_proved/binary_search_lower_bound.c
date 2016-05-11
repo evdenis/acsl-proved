26750e027f374c03e0c08f8a4bebcb4e9225a71a
Denis Efremov <yefremov.denis@gmail.com>
2016-05-11 18:57:00 +0300

    binary_search_lower_bound update

--- not_proved/binary_search_lower_bound.c
+++ not_proved/binary_search_lower_bound.c
@@ -3,7 +3,7 @@
 #endif
 
 /*@ requires \valid(a+(0..n-1));
-    requires \forall integer i, j; 0 <= i <= j < n ==> a[i] <= a[j];
+    requires \forall integer i, j; 0 <= i < j < n ==> a[i] <= a[j];
     assigns \nothing;
     behavior OK:
        assumes \exists integer i; 0 <= i < n && a[i] == key;
@@ -23,13 +23,15 @@ int *bsearch_lower_bound(int a[], unsigned n, int key)
    unsigned left = 0;
    long right = ((long)n) - 1;
 
-   /*@ loop invariant 0 <= left;
-       loop invariant right < n;
+   /*@ loop invariant 0 <= left <= n;
+       loop invariant -1 <= right < n;
+       loop invariant \forall integer i; right < i < n ==> a[i] >= key;
+       loop invariant \forall integer i; 0 <= i < left ==> a[i] < key;
        loop invariant \forall integer i; 0 <= i < n && a[i] == key ==> left <= i;
        loop invariant \forall integer i; 0 <= i < n && a[i] == key ==> a[left] <= key;
        loop variant right - left;
     */
-   while (left <= right) {
+   while (left < right) {
       unsigned m = left + (right - left) / 2;
       int val = a[m];
 
@@ -40,9 +42,20 @@ int *bsearch_lower_bound(int a[], unsigned n, int key)
       }
    }
 
-   if (left > right) {
-      return NULL;
-   } else {
+   if (left == right && a[left] == key) {
       return &a[left];
+   } else {
+      return NULL;
    }
 }
+
+#ifdef OUT_OF_TASK
+#include <stdio.h>
+int main(void)
+{
+   int a[] = {1, 2, 3, 3, 3, 4, 5};
+   printf("%p\n", bsearch_lower_bound(a, sizeof(a) / sizeof(a[0]), 3));
+   return 0;
+}
+#endif
+
