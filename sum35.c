cb3159c84978aa95badafe10c1190dd6db2c7b26
Denis Efremov <efremov@ispras.ru>
2017-02-14 19:21:28 +0300

    sum35 fix naming

--- sum35.c
+++ sum35.c
@@ -3,21 +3,21 @@
 /*@ axiomatic Sum35 {
     logic integer sum35(integer b);
 
-    axiom sum_negative:
+    axiom sum35_negative:
        \forall integer i; i <= 0 ==> sum35(i) == 0;
 
-    axiom sum_iter1:
+    axiom sum35_iter1:
        \forall integer b; b >= 0 && (((b + 1) % 3 != 0) && ((b + 1) % 5 != 0)) ==>
           sum35(b + 1) == sum35(b);
 
-    axiom sum_iter2:
+    axiom sum35_iter2:
        \forall integer b; b >= 0 && (((b + 1) % 3 == 0) || ((b + 1) % 5 == 0)) ==>
           sum35(b + 1) == sum35(b) + (b + 1);
 
-    lemma sum_lower_bound:
+    lemma sum35_lower_bound:
        \forall integer bound; bound >= 0 ==> sum35(0) >= 0;
 
-    lemma sum_increases:
+    lemma sum35_increases:
        \forall integer b1, b2; 0 <= b1 <= b2 ==> sum35(b1) <= sum35(b2);
     }
  */
